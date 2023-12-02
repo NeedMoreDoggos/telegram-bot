1bed1a63922b8b4a44e63168117a1e448bc62e7c
NeedMoreDoggos <donckou@gmail.com>
2023-12-02 07:12:50 +0300

    mod init and starting write telegram client

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,6 +1,29 @@
 package telegram
 
+import "net/http"
+
 type Clien struct {
 	host     string
 	basePath string
+	client   http.Client
+}
+
+func New(host string, token string) Clien {
+	return Clien{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
+
+func (c *Clien) Update() {
+
+}
+
+func (c *Clien) SendMessage() {
+
 }
